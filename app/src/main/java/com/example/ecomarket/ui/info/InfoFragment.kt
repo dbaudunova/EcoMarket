15a32237779522c595554400bb453aaafff6f691
Dilnaz27 <dbaudunova@gmail.com>
2023-12-16 16:35:28 +0600

    complete info screen

--- app/src/main/java/com/example/ecomarket/ui/info/InfoFragment.kt
+++ app/src/main/java/com/example/ecomarket/ui/info/InfoFragment.kt
@@ -11,16 +11,26 @@ import dagger.hilt.android.AndroidEntryPoint
 class InfoFragment : BaseFragment<FragmentInfoBinding, InfoViewModel>() {
 
     override val viewModel: InfoViewModel by viewModels()
+    private val number: String = "0707637341"
 
     override fun initListener() {
         super.initListener()
         binding.btnCall.setOnClickListener {
-            val number = "0707637341"
-            val intent = Intent(Intent.ACTION_CALL)
+            val intent = Intent(Intent.ACTION_DIAL)
             intent.data = Uri.parse("tel:$number")
-
             startActivity(intent)
-
+        }
+        binding.btnWhatsapp.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("smsto:${number}")
+            intent.setPackage("com.whatsapp")
+            startActivity(intent)
+        }
+        binding.btnInstagram.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.instagram.com/dbaudunova")
+            intent.setPackage("com.instagram.android")
+            startActivity(intent)
         }
     }
 
